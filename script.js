6c14060c605bd02bb2f696c1eddc77725a099989
pacuino <frank.senju3@gmail.com>
2024-04-29 05:07:55 -0500

    Fokus 1.2

--- script.js
+++ script.js
@@ -3,18 +3,44 @@ const botonCorto = document.querySelector('.app__card-button--corto');
 const botonEnfoque = document.querySelector('.app__card-button--enfoque');
 const botonLargo = document.querySelector('.app__card-button--largo');
 const banner = document.querySelector('.app__image');
+const titulo = document.querySelector('.app__title');
 
 botonCorto.addEventListener('click', () => {
-    html.setAttribute('data-contexto','descanso-corto');
-    banner.setAttribute('src','./imagenes/descanso-corto.png');
+    cambiarContexto('descanso-corto');
+    
 });
 
 botonEnfoque.addEventListener('click', () => {
-    html.setAttribute('data-contexto','enfoque');
-    banner.setAttribute('src','./imagenes/enfoque.png');
+    cambiarContexto('enfoque');
+    
 });
 
 botonLargo.addEventListener('click', () => {
-    html.setAttribute('data-contexto','descanso-largo');
-    banner.setAttribute('src','./imagenes/descanso-largo.png');
+    cambiarContexto('descanso-largo');
+    
 });
+
+function cambiarContexto(contexto){
+    html.setAttribute('data-contexto',contexto);
+    banner.setAttribute('src',`./imagenes/${contexto}.png`);
+
+    switch (contexto) {
+        case "enfoque":
+            titulo.innerHTML = ` Optimiza tu productividad,<br>
+            <strong class="app__title-strong">sumérgete en lo que importa.</strong>` 
+
+            break;
+        case "descanso-corto":
+            titulo.innerHTML = `¿Que tal un respiro?,
+            <strong class="app__title-strong">¡Haz una pausa corta!</strong>`
+            break;
+
+        case "descanso-largo":
+            titulo.innerHTML = `Hora de volver a la superficie,
+            <strong class="app__title-strong">Haz una pausa larga.</strong>`
+            break;
+            
+        default:
+            break;
+    }
+}
