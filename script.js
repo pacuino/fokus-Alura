a42c0e9cb537992f4d5fcc7355c93e3c54c57301
pacuino <frank.senju3@gmail.com>
2024-05-06 02:11:02 -0500

    Fokus 1.3

--- script.js
+++ script.js
@@ -4,23 +4,41 @@ const botonEnfoque = document.querySelector('.app__card-button--enfoque');
 const botonLargo = document.querySelector('.app__card-button--largo');
 const banner = document.querySelector('.app__image');
 const titulo = document.querySelector('.app__title');
+const botones = document.querySelectorAll('.app__card-button');
+const inputEnfoqueMusica = document.querySelector('#alternar-musica');
+const musica = new Audio('./sonidos/luna-rise-part-one.mp3');
+
+musica.loop = true;
+
+inputEnfoqueMusica.addEventListener('change', ()=> {
+    if(musica.paused){
+        musica.play();
+    }else{
+        musica.pause();  
+    }
+});
 
 botonCorto.addEventListener('click', () => {
     cambiarContexto('descanso-corto');
-    
+    botonCorto.classList.add('active');
 });
 
 botonEnfoque.addEventListener('click', () => {
     cambiarContexto('enfoque');
-    
+    botonEnfoque.classList.add('active');
 });
 
 botonLargo.addEventListener('click', () => {
     cambiarContexto('descanso-largo');
-    
+    botonLargo.classList.add('active');
 });
 
 function cambiarContexto(contexto){
+
+    botones.forEach(function(contexto){
+        contexto.classList.remove('active');
+    })
+
     html.setAttribute('data-contexto',contexto);
     banner.setAttribute('src',`./imagenes/${contexto}.png`);
 
@@ -39,7 +57,7 @@ function cambiarContexto(contexto){
             titulo.innerHTML = `Hora de volver a la superficie,
             <strong class="app__title-strong">Haz una pausa larga.</strong>`
             break;
-            
+
         default:
             break;
     }
